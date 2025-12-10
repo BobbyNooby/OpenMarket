26cd3ab7a398e719aacb475291c8096239f79051
BobbyNooby <theengineering.commandgamer@gmail.com>
2025-12-10 23:28:04 +0000

    chore : elysia drizzle config + lockfile + package update

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
 	out: './drizzle',
-	schema: './src/db/schemas.ts',
+	schema: ['./src/db/schemas.ts', './src/db/auth-schema.ts'],
 	dialect: 'postgresql',
 	dbCredentials: { url: process.env.DATABASE_URL! }
 });
